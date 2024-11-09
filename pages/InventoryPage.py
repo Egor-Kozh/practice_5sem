d5f24ad9039fc236d61b65b9c601845654de1b1b
Egor-Kozh <kozhevnikovegor55@gmail.com>
2024-11-09 09:13:22 +0600

    create_testdeleteoneitem_uppdate_InventoryPage

--- pages/InventoryPage.py
+++ pages/InventoryPage.py
@@ -3,9 +3,13 @@ class InventoryPage:
         self.page = page
         self.add_backpack = page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]")
         self.cart = page.locator("[data-test=\"shopping-cart-link\"]")
+        self.delete_backpack = page.locator("[data-test=\"remove-sauce-labs-backpack\"]")
 
     def add_to_cart(self):
         self.add_backpack.click()
 
     def check_cart(self):
         self.cart.click()
+
+    def delete_cart(self):
+        self.delete_backpack.click()
